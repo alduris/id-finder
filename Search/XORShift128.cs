856a9cfa4b0a96437d22374c1da9700513f8290a
alduris <152772325+alduris@users.noreply.github.com>
2025-01-18 00:42:33 -0500

    Partially fixed XORShift128

--- Search/XORShift128.cs
+++ Search/XORShift128.cs
@@ -5,8 +5,8 @@ namespace FinderMod.Search
     public class XORShift128
     {
         public uint x = 0, y = 0, z = 0, w = 0;
-        const uint MT19937 = 1812433253;
-        public void InitSeed(int seed)
+        const uint MT19937 = 0x6c078965u;
+        public void InitState(int seed)
         {
             x = (uint)seed;
             y = (uint)(MT19937 * x + 1);
@@ -23,33 +23,37 @@ namespace FinderMod.Search
         }
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        private uint _XORShift()
+        private uint NextU32()
         {
             uint t = x ^ (x << 11);
             x = y; y = z; z = w;
             return w = w ^ (w >> 19) ^ t ^ (t >> 8);
         }
-        public uint XORShift() => _XORShift();
+        public uint XORShift() => NextU32();
 
         public int Range(int min, int max)
         {
             if (max - min == 0) return min;
 
-            long minLong = (long)min;
-            long maxLong = (long)max;
-            long r = _XORShift();
+            unchecked
+            {
+                uint minLong = (uint)min;
+                uint maxLong = (uint)max;
+                uint r = NextU32();
 
-            if (max < min)
-                return (int)(minLong - r % (maxLong - minLong));
-            else
-                return (int)(minLong + r % (maxLong - minLong));
+                if (max < min)
+                    return (int)(minLong - r % (maxLong - minLong));
+                else
+                    return (int)(minLong + r % (maxLong - minLong));
+            }
         }
 
-        public float Value => 1.0f - Range(0.0f, 1.0f);
+        public float Value => (NextU32() & 0x7FFFFF) * 1.192093E-07f;
 
         public float Range(float min, float max)
         {
-            return (min - max) * ((float)(_XORShift() << 9) / 0xFFFFFFFF) + max;
+            // Actual code converts to float10 and then back to float, which I think is the cause of the precision problems.
+            return (float)((NextU32() & 0x7FFFFFu) * 1.192093E-07 * (min - max)) + max;
         }
     }
 }
