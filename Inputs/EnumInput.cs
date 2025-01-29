975fbb9917a13fb303537d68fa234372ed4bbca9
alduris <152772325+alduris@users.noreply.github.com>
2025-01-29 02:00:48 -0500

    Import OpComboBox2 and maybe add EnumInput

---
+++ Inputs/EnumInput.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+using Menu.Remix.MixedUI;
+using UnityEngine;
+
+namespace FinderMod.Inputs
+{
+    public class EnumInput<T> : Input<T> where T : Enum
+    {
+        private readonly T init;
+        protected readonly Func<T, string> nameConv = null;
+
+        public EnumInput(string name, T init) : base(name, init)
+        {
+            this.init = init;
+        }
+
+        public EnumInput(string name, T init, Func<T, string> nameConv) : this(name, init)
+        {
+            this.nameConv = nameConv;
+        }
+
+        public override float Height => 24f;
+
+        protected override UIconfig GetElement(Vector2 pos)
+        {
+            var el = new OpResourceSelector(OpUtil.CosmeticBind(value), pos, 160f);
+            if (nameConv != null)
+            {
+                el._itemList = el._itemList.Select(x => new ListItem(x.name, nameConv((T)Enum.Parse(typeof(T), x.name)), x.value)).ToArray();
+            }
+            return el;
+        }
+
+        protected override T GetValue(UIconfig element)
+        {
+            if (element.value != null)
+            {
+                return (T)Enum.Parse(typeof(T), element.value);
+            }
+            return init;
+        }
+    }
+}
---
+++ OpComboBox2.cs
@@ -0,0 +1,55 @@
+﻿global using OpComboBox = MapExporter.Tabs.UI.OpComboBox2;
+global using OpResourceSelector = MapExporter.Tabs.UI.OpResourceSelector2;
+
+using System.Collections.Generic;
+using Menu.Remix.MixedUI;
+using UnityEngine;
+
+namespace MapExporter.Tabs.UI
+{
+
+    /**
+     * OpComboBox that has no transparent background
+     * 
+     * Thanks Henpemaz
+     */
+    public class OpComboBox2 : Menu.Remix.MixedUI.OpComboBox
+    {
+        public OpComboBox2(Configurable<string> config, Vector2 pos, float width, string[] array) : base(config, pos, width, array) { }
+        public OpComboBox2(Configurable<string> config, Vector2 pos, float width, List<ListItem> list) : base(config, pos, width, list) { }
+
+        public override void GrafUpdate(float timeStacker)
+        {
+            base.GrafUpdate(timeStacker);
+            if (_rectList != null && !_rectList.isHidden)
+            {
+                myContainer.MoveToFront();
+
+                for (int j = 0; j < 9; j++)
+                {
+                    _rectList.sprites[j].alpha = 1;
+                }
+            }
+        }
+    }
+
+    public class OpResourceSelector2 : Menu.Remix.MixedUI.OpResourceSelector
+    {
+        public OpResourceSelector2(ConfigurableBase config, Vector2 pos, float width) : base(config, pos, width) { }
+        public OpResourceSelector2(Configurable<string> config, Vector2 pos, float width, SpecialEnum listType) : base(config, pos, width, listType) { }
+
+        public override void GrafUpdate(float timeStacker)
+        {
+            base.GrafUpdate(timeStacker);
+            if (_rectList != null && !_rectList.isHidden)
+            {
+                myContainer.MoveToFront();
+
+                for (int j = 0; j < 9; j++)
+                {
+                    _rectList.sprites[j].alpha = 1;
+                }
+            }
+        }
+    }
+}
