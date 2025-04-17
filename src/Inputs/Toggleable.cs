1028f0ef6731e0b9f3ddec6b927c5c12490dd6d8
alduris <152772325+alduris@users.noreply.github.com>
2025-04-17 09:21:35 -0400

    Toggleable as ISaveInHistory

--- src/Inputs/Toggleable.cs
+++ src/Inputs/Toggleable.cs
@@ -1,11 +1,13 @@
 ﻿using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 using Menu.Remix.MixedUI;
+using Newtonsoft.Json.Linq;
 
 namespace FinderMod.Inputs
 {
-    public class Toggleable<E> : IElement where E : IElement
+    public class Toggleable<E> : IElement, ISaveInHistory where E : IElement
     {
+        protected readonly string name;
         public readonly BoolInput ToggleInput;
         public readonly E Element;
 
@@ -17,11 +19,14 @@ namespace FinderMod.Inputs
 
         public Toggleable(string name, bool toggled, E element)
         {
+            this.name = name;
             ToggleInput = new BoolInput(name, toggled) { hasBias = true };
             Element = element;
         }
 
         public float Height => ToggleInput.Height + (Toggled ? Element.Height + 6f : 0);
+
+        public string SaveKey => name;
 
         public void Create(float x, ref float y, List<UIelement> elements)
         {
@@ -30,6 +35,39 @@ namespace FinderMod.Inputs
             {
                 y -= 6f;
                 Element.Create(x, ref y, elements);
+            }
+        }
+
+        public JObject ToSaveData()
+        {
+            return new JObject
+            {
+                ["toggle"] = ToggleInput.ToSaveData(),
+                ["element"] = (Element is ISaveInHistory saveable) ? saveable.ToSaveData() : null,
+            };
+        }
+
+        public void FromSaveData(JObject data)
+        {
+            ToggleInput.FromSaveData((JObject)data["toggle"]!);
+            if (Element is ISaveInHistory saveable)
+            {
+                saveable.FromSaveData((JObject)data["element"]!);
+            }
+        }
+
+        public IEnumerable<string> GetHistoryLines()
+        {
+            foreach (var item in ToggleInput.GetHistoryLines())
+            {
+                yield return item;
+            }
+            if (Element is ISaveInHistory history)
+            {
+                foreach (var item in history.GetHistoryLines())
+                {
+                    yield return item;
+                }
             }
         }
     }
