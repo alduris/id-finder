a277be1768b52d40be40d6c410ef556f2603089c
alduris <152772325+alduris@users.noreply.github.com>
2025-06-04 03:41:12 -0400

    More test cases in TestOption

--- src/Search/Options/TestOption.cs
+++ src/Search/Options/TestOption.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using FinderMod.Inputs;
+using UnityEngine;
 
 namespace FinderMod.Search.Options
 {
@@ -9,16 +10,44 @@ namespace FinderMod.Search.Options
         {
             elements = [
                 new Label("This is a test option. It does nothing."),
+
                 new Whitespace(),
+
+                new Alternatable<Label>("Alternatable", false, new("This is option A."), new("This is option B.")),
                 new BoolInput("Bool"),
                 new ColorHSLInput("Color HSL"),
+                new ColorLerpInput("Color lerp", Color.red, Color.yellow),
                 new ColorRGBInput("Color RGB"),
                 new EnumInput<TestEnum>("Enum", TestEnum.Apple),
                 new FloatInput("Float"),
                 new Group([new FloatInput("A"), new FloatInput("B"), new FloatInput("C")], "Group"),
                 new HueInput("Hue"),
                 new IntInput("Int", 1, 10),
-                new MultiChoiceInput("Multichoice", ["One", "Two", "Three", "Four", "Five"])
+                new IntManualInput("Int manual", 0),
+                new MultiChoiceInput("Multichoice", ["One", "Two", "Three", "Four", "Five"]),
+                new Switchable<Label>("Switchable", [
+                    ("Option A", new("This is option A.")),
+                    ("Option B", new("This is option B.")),
+                    ("Option C", new("This is option C.")),
+                    ("Option D", new("This is option D.")),
+                    ("Option E", new("This is option E.")),
+                ]),
+                new Toggleable<Label>("Toggleable", true, new Label("This element is toggled on. That is why you can see this text!")),
+
+                new Whitespace(),
+
+                new Group([
+                    new Label("Group nesting test"),
+                    new Group([
+                        new Label("Inner 1"),
+                        new Group([
+                            new Label("Inner 2"),
+                            new Group([
+                                new Label("Inner 3")
+                            ], "inner3")
+                        ], "inner2")
+                    ], "inner1")
+                ], "nesting"),
             ];
         }
 
