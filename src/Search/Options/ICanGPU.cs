d18272e7ff6afb82535464768e76106de7d0a252
alduris <152772325+alduris@users.noreply.github.com>
2025-06-21 00:36:12 -0400

    ICanGPU progress push, unfinished

---
+++ src/Search/Options/ICanGPU.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+namespace FinderMod.Search.Options
+{
+    /// <summary>
+    /// Interface for search options that can use the GPU
+    /// </summary>
+    public interface ICanGPU
+    {
+        /// <summary>
+        /// Returns GPU inputs to be loaded onto the created compute shaders.
+        /// </summary>
+        /// <returns></returns>
+        public GPUInput[] GetGPUInputs();
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public ComputeShader CreateShader();
+
+        /// <summary>
+        /// Struct representing a single input. Transferred to the GPU.
+        /// </summary>
+        public struct GPUInput
+        {
+            /// <summary>
+            /// Whether the input is enabled. In the GPU, treated like an integer.
+            /// </summary>
+            public bool enabled;
+
+            /// <summary>
+            /// The value of the input.
+            /// </summary>
+            public float value;
+
+            /// <summary>
+            /// The bias of the input.
+            /// </summary>
+            public int bias;
+        }
+    }
+}
